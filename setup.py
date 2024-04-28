b7a404bd5db3d0d60f29cfd0f73b29151e910a7b
axiom <157246458+axiom50@users.noreply.github.com>
2024-04-28 21:43:11 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='crypt',
+    version='0.1',
+    packages=['crypt'],
+    url='https://github.com/axiom50/crypt',
+    author='axiom50',
+    author_email='exiom50@gmail.com',
+    description='crypt and uncrypt text',
+    install_requires=[],
+)
